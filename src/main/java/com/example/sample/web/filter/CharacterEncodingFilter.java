c3863607d7e672d7af4da0683c195a547e929ef5
Kohei Hara <koheihara@H39451623.local>
2025-09-12 14:52:26 +0900

    chore: add filter

---
+++ src/main/java/com/example/sample/web/filter/CharacterEncodingFilter.java
@@ -0,0 +1,29 @@
+package com.example.sample.web.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    if (req.getCharacterEncoding() == null) {
+      req.setCharacterEncoding("UTF-8");
+    }
+    res.setCharacterEncoding("UTF-8");
+
+    chain.doFilter(request, response);
+  }
+}
